2f830c43ea7274e3707134ca18620451eedad7b6
NotMehul <47142723+not-mehul@users.noreply.github.com>
2025-06-24 10:39:09 -0500

    Updated AnalysisResults.tsx

--- src/components/AnalysisResults.tsx
+++ src/components/AnalysisResults.tsx
@@ -513,7 +513,7 @@ export const AnalysisResults: React.FC<AnalysisResultsProps> = ({
 
         <div className="flex items-left justify-left text-sm text-blue-600 dark:text-blue-400">
           <ul>
-            <li>Camera Data has been cleaned to remove IP addresses, MAC addresses, dates, and common words.</li>
+            <li>Camera Data has been cleaned to remove IP addresses, MAC addresses, dates, and common words. This data has also been checked against observed third-party camera database containing {thirdPartyCameras.length} models.</li>
             {thirdPartyCameras.length > 0 && (
               <li>This data has also been checked against observed third-party camera database containing {thirdPartyCameras.length} models.</li>
             )}
